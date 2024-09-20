30ac4676354e571e10254e81a74007291420dca9
felix <schwaigerf748@gmail.com>
2024-09-20 11:04:05 +0200

    test_schalter.py

---
+++ test_schalter.py
@@ -0,0 +1,21 @@
+from gpiozero import LED,Button
+import time
+
+button=Button(24)
+
+led1 = LED(18)
+led2 = LED(22)
+
+led_state = False
+
+while True:
+    
+    if button.is_pressed:
+        led_state = not led_state
+        led1.value = led_state
+        led2.value = not led_state
+        time.sleep(0.1)  
+    if button==0:
+        led2.value=led_state
+        led1.value =led_state
+
